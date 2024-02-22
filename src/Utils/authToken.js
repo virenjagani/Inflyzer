586b248ec235186ecacd433e84bb97acc4f4b5a3
Virenkumar Jagani <143466608+virenjagani@users.noreply.github.com>
2024-02-21 22:13:59 -0500

    Auth

--- src/Utils/authToken.js
+++ src/Utils/authToken.js
@@ -3,7 +3,7 @@ import { store } from "../redux/store";
 
 function authToken() {
   if (store.getState().auth.token) {
-
+    // return takon with auth
     return store.getState().auth.token;
   }
   
